9da1e65a69fd23947cb9858d34618e42db3267b4
Marlene <marlene.goedecke@gmail.com>
2025-04-25 19:20:15 +0200

    initial commit

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './button';
+
+const meta = {
+  title: 'Component/Button',
+  component: Button,
+  args: {
+    children: 'Button',
+    variant: 'primary',
+    disabled: false,
+  },
+  argTypes: {
+    disabled: {
+      control: 'boolean'
+    },
+    variant: {
+      control: 'select'
+    }
+  }
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {};
+
+export const Secondary: Story = {
+  args: {
+    children: 'Secondary',
+    variant: 'secondary',
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    children: 'Destructive',
+    variant: 'destructive',
+  },
+};
+
+export const Dark: Story = {
+  parameters: {
+    themes: {
+      themesOverride: 'darl'
+    }
+  }
+}
+
+export const Mobile: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'mobile1'
+    }
+  }
+}
